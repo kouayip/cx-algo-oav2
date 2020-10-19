83602424e41e56673e6b5a71ac7fa22a27a8b63e
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2020-10-19 09:11:46 +0200

    Sorting by selection

---
+++ e03.js
@@ -0,0 +1,19 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      const help = array[i];
+      array[i] = array[min];
+      array[min] = help;
+    }
+  }
+
+  return array;
+}
+
+console.log(selectionSort([1, 3, 6, 2, 9, 0, 4, 6]));
